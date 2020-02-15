1c660c14255c1201faeaa81a1758e6b29398bbfc
Prajwal V Bharadwaj <prajwal.bharadwaj2@mindtree.com>
2020-02-15 17:34:01 +0530

    small changes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const port = process.env.PORT || 5000
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const port = process.env.PORT || 5000;
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 server.listen(port, () => {
-  console.log('JSON Server is running')
-})
+  console.log("JSON Server is running on port ", port);
+});
